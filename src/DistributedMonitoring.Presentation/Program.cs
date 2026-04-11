575fe2542bf82daf524c7fe138383744e74a73b2
astorniolo <astorniolo@gmail.com>
2026-04-11 20:29:06 -0300

    fix problema en  Program.cs

--- src/DistributedMonitoring.Presentation/Program.cs
+++ src/DistributedMonitoring.Presentation/Program.cs
@@ -40,7 +40,7 @@ static class Program
         var logService = serviceProvider.GetRequiredService<ILogService>();
         logService.LogSystem("Aplicacion iniciada");
 
-        System.Windows.Forms.ApplicationConfiguration.Initialize();
+        ApplicationConfiguration.Initialize();
         System.Windows.Forms.Application.Run(new MainForm(serviceProvider));
     }
 }
